b0b058be5229e81eb8f0100ffa2e092a43bc8553
Alex Panaev <panaev.as@gmail.com>
2023-05-19 17:19:17 +0400

    updated Utils class: added httpClient, ObjectMapper, getUrl()

--- src/main/java/com/netology/Utils.java
+++ src/main/java/com/netology/Utils.java
@@ -1,4 +1,31 @@
 package com.netology;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import java.io.IOException;
+
 public class Utils {
+
+    //HTTP client settings for sending requests
+    private static CloseableHttpClient httpClient = HttpClientBuilder.create()
+                                                      .setDefaultRequestConfig(RequestConfig.custom()
+                                                      .setConnectTimeout(5000)
+                                                      .setSocketTimeout(30000)
+                                                      .setRedirectsEnabled(false)
+                                                      .build())
+                                                      .build();
+
+    private static ObjectMapper objectMapper = new ObjectMapper();
+
+    public static String getUrl(String url) throws IOException {
+        CloseableHttpResponse response = httpClient.execute(new HttpGet(url));
+        NasaObject nasaObject = objectMapper.readValue(response.getEntity()
+                                                               .getContent(),NasaObject.class);
+        return nasaObject.getUrl();
+    }
 }
