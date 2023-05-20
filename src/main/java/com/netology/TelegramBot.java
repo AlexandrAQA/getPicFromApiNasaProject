92cdcc0d2fc9763ff3b1a10cfc374a3b558ab98d
Alex Panaev <panaev.as@gmail.com>
2023-05-20 19:23:39 +0400

    added onUpdateReceived + updated chat_id

--- src/main/java/com/netology/TelegramBot.java
+++ src/main/java/com/netology/TelegramBot.java
@@ -7,12 +7,14 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+
 public class TelegramBot extends TelegramLongPollingBot {
 
     public static final String BOT_TOKEN = "5893916393:AAGQmCIxQu4wgeFKXqZdH8isk-2DO2dkM_g";
     public static final String BOT_USERNAME = "myJavaNasaBot_bot";
     public static final String API_KEY = "1DdGzcwrfTKNfeHMboP8BEglPVTspG0um8kPN4Dn";
-    public static final String chat_id = "";
+    public static long chat_id;
 
     //URI for connection with private api_key
     public static final String URI =
@@ -33,11 +35,6 @@ public class TelegramBot extends TelegramLongPollingBot {
         return BOT_TOKEN;
     }
 
-    @Override
-    public void onUpdateReceived(Update update) {
-
-    }
-
     private void sendMessage(String message){
         SendMessage sendMessage = new SendMessage();
         sendMessage.setChatId(chat_id);
@@ -46,6 +43,28 @@ public class TelegramBot extends TelegramLongPollingBot {
             execute(sendMessage);
         } catch (TelegramApiException exception){
             exception.printStackTrace();
+        }
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage()) {
+            chat_id = update.getMessage().getChatId();
+            switch (update.getMessage().getText()) {
+                case "/help":
+                    sendMessage("Hello, I'm a NASA bot! I send links to pictures upon request." +
+                            "Pictures on the NASA website are updated once a day.");
+                    break;
+                case "/give":
+                    try {
+                        sendMessage(Utils.getUrl(URI));
+                    } catch (IOException e) {
+                        throw new RuntimeException(e);
+                    }
+                    break;
+                default:
+                    sendMessage("I do not understand you. Please try again");
+            }
         }
     }
 }
